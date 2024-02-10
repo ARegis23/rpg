31cf44c3a9058bf5d4d9167f46fec5b921594c0a
ARegis23 <113563382+ARegis23@users.noreply.github.com>
2024-02-09 22:45:05 -0300

    atualizacoes da noite 09/02/24

---
+++ FICHA_ONDELINE/loginM.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="stylelogins.css">
+        <title> LOGIN </title>
+    </head>
+
+    <body>
+        <div class="login">
+            <h1> Bem - Vindo MESTRE </h1>
+            <br>
+            <h2> Faça seu login </h2>
+        </div>
+
+        <div class="login">
+            <form class="">
+
+                <div class="">
+                    <label for="username"> Email </label>
+                    <input type="text" id="username" name="username" placeholder="email@example.com" required>
+                </div>
+
+                <div class="">
+                    <label for="senha"> Senha </label>
+                    <input type="password" id="senha" name="senha" placeholder="********" required>
+                </div>
+                
+                <input type="submit" value="Login">
+            
+            </form>
+
+            <div>
+                <a class="" href="cadloginM.html"> Novo Aqui? Cadastra-se! </a>
+                <a class="" href="esqueceu.html"> Esqueceu a Senha? </a>
+            </div>
+        </div>
+
+        <div class="rodape">
+            <footer>
+                <a href="home.html"><strong> Voltar </strong></a>
+            </footer>
+        </div>
+    </body>
+</html>
